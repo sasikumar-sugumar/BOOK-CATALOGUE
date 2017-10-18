f4520855b5448da114a61eef551835fd205de1a4
SASIKUMAR SUGUMAR <sasisugumar@gmail.com>
2017-10-17 22:27:45 -0500

    Initial Upload

---
+++ BookDomain/src/main/java/com/sskez/book/catalogue/BookDomainApplication.java
@@ -0,0 +1,12 @@
+package com.sskez.book.catalogue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookDomainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookDomainApplication.class, args);
+	}
+}
